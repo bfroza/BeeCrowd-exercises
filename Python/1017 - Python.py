d6f455cc05fa0c5133a3d5bd9c8f6f136524395c
bfroza <032758@aluno.uricer.edu.br>
2023-06-30 22:07:47 -0300

    Foi

--- Python/1017 - Python.py
+++ Python/1017 - Python.py
@@ -1,5 +1,5 @@
 horas = int(input())
 velocidade_med = int(input())
 
-gasto_combu = horas*velocidade_med
-print(f'{gasto_combu/12 :.3f}')
+gasto_combustivel = horas*velocidade_med
+print(f'{gasto_combustivel/12 :.3f}')
